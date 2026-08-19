62809b7e76a8e0bd5f6b28a689598d9c77c4eabc
YASMIN MAÇANEIRO DE SOUSA <yasmin_sousa@estudante.sesisenai.org.br>
2026-08-18 21:13:35 -0300

    triangulo

--- src/test/java/TrianguloTest.java
+++ src/test/java/TrianguloTest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import org.example.Triangulo;
+import org.junit.jupiter.api.Test;
 
-public class TrianguloTest {
+// Importa as asserções estáticas do JUnit 5.
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrianguloTest {
+// Os métodos de teste ficam aqui.
+
+    @Test
+    void calcularPerimetroDeveSomarOsTresLados() {
+        // Arrange
+        Triangulo triangulo = new Triangulo(3, 4, 5);
+        // Act
+        double perimetro = triangulo.calcularPerimetro();
+        // Assert
+        assertEquals(12.0, perimetro);
+    }
+
+    @Test void ladoNegativoDeveLancarExcecao(){
+        assertThrows( IllegalArgumentException.class,()-> new Triangulo(-1, 4, 5));
+    }
+
+    @Test
+    void triangulo3_4_5DeveTerLadosCorretos() {
+        Triangulo t = new Triangulo(3, 4, 5);
+        assertAll(
+                () -> assertEquals(3.0, t.getLadoA()),
+                () -> assertEquals(4.0, t.getLadoB()),
+                () -> assertEquals(5.0, t.getLadoC())
+        );
+    }
 }
